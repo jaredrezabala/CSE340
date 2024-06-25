3d133405d8ad82a1b9bf59d94753826cdf623c15
jaredrezabala <rezabalajared@gmail.com>
2024-06-24 21:05:25 -0500

    styling finish - error handling exercises complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
  *************************/
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const utilities = require("./utilities/");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
@@ -24,10 +25,31 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 //Invetory routes
 app.use("/inv", inventoryRoute);
-
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?";
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
