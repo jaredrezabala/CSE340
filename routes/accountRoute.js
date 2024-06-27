49dd91b8dccd93ea8cf375bbb8a4e68db91be103
jaredrezabala <rezabalajared@gmail.com>
2024-06-27 00:01:55 -0500

    login and register view deliver - register data succesfully submitted to database

---
+++ routes/accountRoute.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities/");
+const accountController = require("../controllers/accountController");
+
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+);
+router.post(
+  "/registerAcc",
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+module.exports = router;
